ce4b7af75108497114d0906aa216774b307e9723
hars321 <54657843+hars321@users.noreply.github.com>
2020-12-21 18:03:00 +0530

    try 2

--- src/Monaco/index.js
+++ src/Monaco/index.js
@@ -66,14 +66,4 @@ class App extends React.Component {
 }
 
 export default App;
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
